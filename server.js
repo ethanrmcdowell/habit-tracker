19f64b0da3112ed7f2564d4fa4f4a5683700d72d
Ethan McDowell <ethan.r.mcdowell@gmail.com>
2021-06-20 14:18:13 -0400

    Heroku deployment changes

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+const path = require('path');
+const publicPath = path.join(__dirname, '/client/public');
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
